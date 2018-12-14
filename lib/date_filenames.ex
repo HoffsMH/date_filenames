1b7c729d3a04781c9aafea61db7c88eb436d7d57
HoffsMH <hecker.matthew@gmail.com>
2018-12-14 17:31:22 -0500

    initial commit

---
+++ lib/date_filenames.ex
@@ -0,0 +1,18 @@
+defmodule DateFilenames do
+  @moduledoc """
+  Documentation for DateFilenames.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DateFilenames.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/date_filenames_test.exs
@@ -0,0 +1,8 @@
+defmodule DateFilenamesTest do
+  use ExUnit.Case
+  doctest DateFilenames
+
+  test "greets the world" do
+    assert DateFilenames.hello() == :world
+  end
+end
